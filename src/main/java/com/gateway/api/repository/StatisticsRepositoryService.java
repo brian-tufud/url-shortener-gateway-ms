ac4573ad5dcdd301636d3ce0ddf069b97db99774
Brian <brian.cabrera@tufud.co>
2024-04-14 19:19:49 -0300

    [REPOSITORY] implemented getShortURLStatistics and getLongURLStatistics in StatisticsRepositoryService

--- src/main/java/com/gateway/api/repository/StatisticsRepositoryService.java
+++ src/main/java/com/gateway/api/repository/StatisticsRepositoryService.java
@@ -1,18 +1,48 @@
 package com.gateway.api.repository;
 
+import java.net.URI;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.core5.net.URIBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.gateway.api.response.LongURLStatisticsResponse;
 import com.gateway.api.response.ShortURLStatisticsResponse;
+import com.gateway.api.utils.Constants;
+import com.gateway.api.utils.RestClientService;
 
 @Service
 public class StatisticsRepositoryService {
 
-    public ShortURLStatisticsResponse getShortURLStatistics(String shortURL) {
-        return null;
+    @Autowired
+    private RestClientService restClientService;
+
+    @Autowired
+    private Environment env;
+
+    public ShortURLStatisticsResponse getShortURLStatistics(String shortURL) throws Exception {
+        
+        String baseURL = env.getProperty(Constants.STATISTICS_BASE_URL);
+
+        HttpGet request = new HttpGet(baseURL + "/statistics/short_url/" + shortURL);
+
+        return restClientService.executeRequest(request, ShortURLStatisticsResponse.class);
     }
 
-    public LongURLStatisticsResponse getLongURLStatistics(String longURL) {
-        return null;
+    public LongURLStatisticsResponse getLongURLStatistics(String longURL) throws Exception {
+        
+        String baseURL = env.getProperty(Constants.STATISTICS_BASE_URL);
+
+        HttpGet request = new HttpGet(baseURL + "/statistics/long_url");
+
+        URI uri = new URIBuilder(request.getUri())
+                .addParameter("long_url", longURL)
+                .build();
+
+        request.setUri(uri);
+
+        return restClientService.executeRequest(request, LongURLStatisticsResponse.class);
     }
 }
