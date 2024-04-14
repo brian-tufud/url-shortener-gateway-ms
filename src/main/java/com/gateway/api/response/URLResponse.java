b4b721f814e391b5dba9214e3cef659f93dd3d51
Brian <brian.cabrera@tufud.co>
2024-04-14 19:18:17 -0300

    [RESPONSE] created URLResponse

---
+++ src/main/java/com/gateway/api/response/URLResponse.java
@@ -0,0 +1,26 @@
+package com.gateway.api.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.SerializedName;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class URLResponse {
+
+    @SerializedName("short_url")
+    @JsonProperty("short_url")
+    private String shortURL;
+
+    @SerializedName("long_url")
+    @JsonProperty("long_url")
+    private String longURL;
+}
