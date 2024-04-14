68ce2fafb15488df67c6590ab8d8356386d20ed1
Brian <brian.cabrera@tufud.co>
2024-04-14 19:22:03 -0300

    [CONTROLLER] modified endpoint shortenURL in ShortURLController

--- src/main/java/com/gateway/api/controller/ShortURLController.java
+++ src/main/java/com/gateway/api/controller/ShortURLController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import com.gateway.api.request.LongURLRequest;
 import com.gateway.api.response.CrudStatusResponse;
+import com.gateway.api.response.URLResponse;
 import com.gateway.api.response.CrudStatusResponse.CrudStatus;
 import com.gateway.api.service.ShortURLService;
 import com.gateway.api.utils.UtilsService;
@@ -29,15 +30,13 @@ public class ShortURLController {
     }
 
     @PostMapping()
-    public ResponseEntity<CrudStatusResponse> shortenURL(HttpServletRequest request,
+    public ResponseEntity<URLResponse> shortenURL(HttpServletRequest request,
         @RequestBody LongURLRequest body) throws Exception {
 
-        shortURLService.shortenURL(body);
-
-        CrudStatusResponse response = CrudStatusResponse.builder().status(CrudStatus.CREATED).build();
+        URLResponse url = shortURLService.shortenURL(body);
 
         HttpHeaders responseHeaders = utilsService.getResponseHeaders();
-        return ResponseEntity.ok().headers(responseHeaders).body(response);
+        return ResponseEntity.ok().headers(responseHeaders).body(url);
     }
 
     @DeleteMapping("/{short_url}")
