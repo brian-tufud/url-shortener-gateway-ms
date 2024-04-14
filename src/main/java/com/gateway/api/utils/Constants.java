f663762a7eaa3c576a9ad749d8df4b6c2ab7c215
Brian <brian.cabrera@tufud.co>
2024-04-14 19:20:08 -0300

    [UTILS] added new constants in Constants.java

--- src/main/java/com/gateway/api/utils/Constants.java
+++ src/main/java/com/gateway/api/utils/Constants.java
@@ -2,11 +2,27 @@ package com.gateway.api.utils;
 
 public class Constants {
     
-    public static final String SHORT_URL_PREFIX = "http://localhost:8080/";
+    public static final String SHORT_URL_PREFIX = "https://879e-2800-a4-2296-a500-2471-f02e-eb81-458b.ngrok-free.app/";
 
     public static final String READ_BASE_URL = "read_base_url";
 
     public static final String WRITE_BASE_URL = "write_base_url";
 
     public static final String STATISTICS_BASE_URL = "statistics_base_url";
+
+    public static final String STATISTICS_QUEUE = "statistics_queue";
+
+    public static final String[] IP_HEADER_CANDIDATES = {
+        "X-Forwarded-For",
+        "Proxy-Client-IP",
+        "WL-Proxy-Client-IP",
+        "HTTP_X_FORWARDED_FOR",
+        "HTTP_X_FORWARDED",
+        "HTTP_X_CLUSTER_CLIENT_IP",
+        "HTTP_CLIENT_IP",
+        "HTTP_FORWARDED_FOR",
+        "HTTP_FORWARDED",
+        "HTTP_VIA",
+        "REMOTE_ADDR"
+    };
 }
