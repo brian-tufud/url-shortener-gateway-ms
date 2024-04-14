74f286fd5d9aceacc34e5e46e65eaa5b551e8e25
Brian <brian.cabrera@tufud.co>
2024-04-14 17:24:38 -0300

    [UTILS] added SHORT_URL_PREFIX, READ_BASE_URL, WRITE_BASE_URL and STATISTICS_BASE_URL to Constants.java

--- src/main/java/com/gateway/api/utils/Constants.java
+++ src/main/java/com/gateway/api/utils/Constants.java
@@ -2,4 +2,11 @@ package com.gateway.api.utils;
 
 public class Constants {
     
+    public static final String SHORT_URL_PREFIX = "http://localhost:8080/";
+
+    public static final String READ_BASE_URL = "read_base_url";
+
+    public static final String WRITE_BASE_URL = "write_base_url";
+
+    public static final String STATISTICS_BASE_URL = "statistics_base_url";
 }
