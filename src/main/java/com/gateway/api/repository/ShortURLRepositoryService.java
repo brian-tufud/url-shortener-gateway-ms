227ee29e11e2b3aade928cd9bbff0c09561b661d
Brian <brian.cabrera@tufud.co>
2024-04-14 17:25:14 -0300

    [REPOSITORY] created methods getLongURL, shortenURL and deleteURL in ShortURLRepositoryService

--- src/main/java/com/gateway/api/repository/ShortURLRepositoryService.java
+++ src/main/java/com/gateway/api/repository/ShortURLRepositoryService.java
@@ -1,21 +1,62 @@
 package com.gateway.api.repository;
 
+import java.nio.charset.StandardCharsets;
+
+import org.apache.hc.client5.http.classic.methods.HttpDelete;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.gateway.api.request.LongURLRequest;
+import com.gateway.api.response.CrudStatusResponse;
+import com.gateway.api.utils.Constants;
+import com.gateway.api.utils.RestClientService;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.GsonBuilder;
 
 @Service
 public class ShortURLRepositoryService {
 
-    public String getLongURL(String shortURL) {
-        return null;
+    @Autowired
+    private RestClientService restClientService;
+
+    @Autowired
+    private Environment env;
+
+    public String getLongURL(String shortURL) throws Exception {
+        
+        String baseURL = env.getProperty(Constants.READ_BASE_URL);
+
+        HttpGet request = new HttpGet(baseURL + "/url/" + shortURL);
+
+        return restClientService.executeRequest(request, String.class);
     }
 
-    public String shortenURL(LongURLRequest body) {
-        return null;
+    public String shortenURL(LongURLRequest body) throws Exception {
+        
+        String baseURL = env.getProperty(Constants.WRITE_BASE_URL);
+
+        HttpPost request = new HttpPost(baseURL + "/url");
+
+        String LongURLRequest = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create().toJson(body);
+
+        request.setEntity(new StringEntity(LongURLRequest, StandardCharsets.UTF_8));
+        request.addHeader("Content-Type", "application/json");
+
+        return restClientService.executeRequest(request, String.class);
     }
 
-    public void deleteURL(String shortURL) {
-        return;
+    public CrudStatusResponse deleteURL(String shortURL) throws Exception {
+        
+        String baseURL = env.getProperty(Constants.WRITE_BASE_URL);
+
+        HttpDelete request = new HttpDelete(baseURL + "/url/" + shortURL);
+
+        return restClientService.executeRequest(request, CrudStatusResponse.class);
     }
 }
