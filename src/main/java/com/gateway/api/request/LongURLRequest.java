7a938ce14b1ae5f8abf52169536d3c137a12d718
Brian <brian.cabrera@tufud.co>
2024-04-14 19:18:36 -0300

    [REQUEST] modified LongURLRequest

--- src/main/java/com/gateway/api/request/LongURLRequest.java
+++ src/main/java/com/gateway/api/request/LongURLRequest.java
@@ -1,6 +1,8 @@
 package com.gateway.api.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.SerializedName;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,7 +13,8 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @SuperBuilder
 public class LongURLRequest {
-    
+
+    @SerializedName("long_url")
     @JsonProperty("long_url")
     private String longURL;
     
