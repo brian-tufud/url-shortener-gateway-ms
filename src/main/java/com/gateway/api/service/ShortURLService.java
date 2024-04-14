2c1266b95da5e1e68c4349eff02c57c94d2cea77
Brian <brian.cabrera@tufud.co>
2024-04-14 19:21:33 -0300

    [SERVICE] implemented getLongURL and shortenURL

--- src/main/java/com/gateway/api/service/ShortURLService.java
+++ src/main/java/com/gateway/api/service/ShortURLService.java
@@ -5,22 +5,30 @@ import org.springframework.stereotype.Service;
 
 import com.gateway.api.repository.ShortURLRepositoryService;
 import com.gateway.api.request.LongURLRequest;
+import com.gateway.api.response.URLResponse;
+import com.gateway.api.utils.UtilsService;
 
 @Service
 public class ShortURLService {
 
     @Autowired
+    private UtilsService utilsService;
+
+    @Autowired
     private ShortURLRepositoryService shortURLRepositoryService;
 
-    public String getLongURL(String shortURL) throws Exception {
+    public URLResponse getLongURL(String shortURL) throws Exception {
         return shortURLRepositoryService.getLongURL(shortURL);
     }
 
-    public String shortenURL(LongURLRequest body) throws Exception {
-        return shortURLRepositoryService.shortenURL(body);
+    public URLResponse shortenURL(LongURLRequest body) throws Exception {
+        URLResponse response = shortURLRepositoryService.shortenURL(body);
+
+        return utilsService.getShortURLResponse(response);
     }
 
     public void deleteURL(String shortURL) throws Exception {
         shortURLRepositoryService.deleteURL(shortURL);
     }
+
 }
